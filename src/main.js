24edb927130be81f2c5dcc0d74b40afc0e877a99
pavel <pavel@linnovate.net>
2016-09-13 11:52:01 +0300

    ud version

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import SoundCloudLikePlayer from './player';
 
 let player = new SoundCloudLikePlayer({
     container: document.getElementById('example'),
+    playerClientId: 'cUa40O3Jg3Emvp6Tv4U6ymYYO50NUGpJ',
     clientId: 'cf92370f6c9691fab24bdf6791b57d61',
     autoplay: false,
-    redirectUri: 'http://localhost:3001/callback.html'
+    nativePlayer: true,
+    redirectUri: 'http://localhost:3001/callback.html',
+    apiUrl: 'http://52.169.29.134/sc-tpa/api'
+    
 });
-player.on('ended', () => player.next());
+
+/*player.player.configure('_baseUrl', 'http://52.169.29.134/sc-tpa/api');
+player.on('ended', () => player.next());*/
+//player.resolve('https://api.soundcloud.com/playlists/218563529');
 player.resolve('https://soundcloud.com/bigbabydram/sets/d-r-a-m-friends');
 //player.search({q: 'buskers', limit: 20, offset: 10});
